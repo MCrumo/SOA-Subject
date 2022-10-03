00b9d7bf10ccee5ef6d2252d4df7ded0c680a136
MCrumo <miquel1rr@gmail.com>
2022-10-03 10:43:11 +0200

    new file added

---
+++ zeos_NEW/user.c
@@ -0,0 +1,33 @@
+#include <libc.h>
+
+char buff[24];
+
+int pid;
+extern int zeos_ticks;
+extern int gettime();
+extern void perror();
+
+int __attribute__ ((__section__(".text.main"))) main(void) {
+
+    gettime();
+    
+    /*write*/
+    if(write(1, "\n***** \n", 9) == -1) perror();
+    char * buff;
+    buff = "\n Test de write a user.c\n";
+    if(write(1, buff, strlen(buff)) == -1) perror();
+    if(write(1, "***** \n", 9) == -1) perror();
+    
+
+    /*gettime*/
+    buff = "\n Test de gettime a user.c\n";
+    if(write(1, buff, strlen(buff)) == -1) perror();
+    itoa(gettime(), buff); 
+    if(write(1, buff, strlen(buff)) == -1) perror();
+    buff = " resultat de gettime())\n";
+    if(write(1, buff, strlen(buff)) == -1) perror();
+    buff = "***** \n\n";
+    if(write(1, buff, strlen(buff)) == -1) perror();
+    
+    while(1) {}
+}
